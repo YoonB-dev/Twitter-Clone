4bbadbacd26f5572f33844da1fcb13df64e32126
Yoon <151509222+yoonB-dev@users.noreply.github.com>
2024-08-22 18:27:17 +0900

    twitter

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,12 @@
-import { auth } from "../firebase";
+import styled from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div``;
 
 export default function Home(){
-    const LogOut = () =>{
-        auth.signOut();
-    }
-
     return(
-        <h1>
-            <button onClick={LogOut}>Log Out</button>
-        </h1>
+        <Wrapper>
+            <PostTweetForm />
+        </Wrapper>
     );
 }
